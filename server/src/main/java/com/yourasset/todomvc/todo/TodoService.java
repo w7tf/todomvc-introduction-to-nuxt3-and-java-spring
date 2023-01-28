b91befb0844ef784e70d6cd795546e64952a8bf0
warum7 <warum7@warum7.com>
2023-01-28 13:13:23 +0100

    feat: add create & delete service operations

--- server/src/main/java/com/yourasset/todomvc/todo/TodoService.java
+++ server/src/main/java/com/yourasset/todomvc/todo/TodoService.java
@@ -1,16 +1,34 @@
 package com.yourasset.todomvc.todo;
 
 import java.util.List;
-import org.springframework.stereotype.Service;
+import java.util.Optional;
 
+import org.springframework.stereotype.Service;
 
 @Service
 public class TodoService {
 
+    private final TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
     public List<Todo> getAllTodos() {
-        return List.of(new Todo(
-                "Learn Spring Boot",
-                false));
+        return todoRepository.findAll();
+    }
+
+    public List<Todo> getAllCompletedTodos() {
+        Optional<Todo> completedTodos = todoRepository.findCompleted(true);
+        return completedTodos.isPresent() ? List.of(completedTodos.get()) : List.of();
+    }
+
+    public void createTodo(Todo todo) {
+        todoRepository.save(todo);
+    }
+
+    public void deleteCompletedTodos() {
+        todoRepository.deleteAllInBatch(getAllCompletedTodos());
     }
 
 }
