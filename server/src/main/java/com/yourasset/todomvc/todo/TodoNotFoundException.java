ed89e0f2477043e31b8ecdcd35ffecec81692d58
warum7 <warum7@warum7.com>
2023-01-28 13:22:56 +0100

    feat: create custom exception

---
+++ server/src/main/java/com/yourasset/todomvc/todo/TodoNotFoundException.java
@@ -0,0 +1,21 @@
+package com.yourasset.todomvc.todo;
+
+public class TodoNotFoundException extends Exception {
+
+    public TodoNotFoundException() {
+        super();
+    }
+
+    public TodoNotFoundException(String message) {
+        super(message);
+    }
+
+    public TodoNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public TodoNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+}
