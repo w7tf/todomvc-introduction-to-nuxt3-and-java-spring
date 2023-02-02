d87b0f98f73c4287bce9eda272e0442eb2d8ae7e
warum7 <warum7@warum7.com>
2023-02-02 11:47:52 +0100

    refactor: ultilize NuxtPages for switching layout

---
+++ frontend/pages/app/all.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+const { data: todos } = await useFetch('http://localhost:8080/api/v1/todos')
+
+</script>
+
+
+<template>
+
+    <TodoItemsWrapper v-for="todo in todos" :key="todo.id">
+        <TodoItem :id="todo.id" :title="todo.title" :isCompleted="todo.completed"  />
+    </TodoItemsWrapper>
+    
+</template>
---
+++ frontend/pages/app/completed.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+const { data: todos } = await useFetch('http://localhost:8080/api/v1/todos/completed')
+
+
+</script>
+
+
+<template>
+
+    <TodoItemsWrapper v-for="todo in todos" :key="todo.id">
+        <TodoItem :id="todo.id" :title="todo.title" :isCompleted="todo.completed"  />
+    </TodoItemsWrapper>
+    
+</template>
