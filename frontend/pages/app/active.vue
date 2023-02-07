9abc07e371771f1576a57c56bed0fcbf059a05b8
warum7 <warum7@warum7.com>
2023-02-07 15:29:32 +0100

    fix: jitter when adding or removing todos

--- frontend/pages/app/active.vue
+++ frontend/pages/app/active.vue
@@ -1,20 +1,16 @@
 <script setup lang="ts">
 
-const { data: todos, error, pending } = await useFetch<Todo[]>('http://localhost:8080/api/v1/todos/active')
+const { data: todos, error } = await useFetch<Todo[]>('http://localhost:8080/api/v1/todos/active')
 
 if (error.value) {
     navigateTo('/error')
 }
-
-
-
-
 </script>
 
 
 <template>
 
-    <TodoItemsWrapper v-if="!pending" v-for="todo in todos" :key="todo.id">
+    <TodoItemsWrapper  v-for="todo in todos" :key="todo.id">
         <TodoItem :id="todo.id" :title="todo.title" :isCompleted="todo.completed" />
     </TodoItemsWrapper>
 
