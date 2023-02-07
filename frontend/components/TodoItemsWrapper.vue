5dfb3e702f19fd3b77ed300b15117040e618379f
warum7 <warum7@warum7.com>
2023-02-07 13:46:37 +0100

    refactor: tailwind changes

--- frontend/components/TodoItemsWrapper.vue
+++ frontend/components/TodoItemsWrapper.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="mx-auto z-10">
-        <div class="border-gray-200 border-t-[1px] mx-auto py-4 px-2">
+        <div class="border-gray-200 border-t-[1px] mx-auto py-4 px-2" >
             <slot></slot>
         </div>
     </div>
