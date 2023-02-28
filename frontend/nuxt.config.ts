4dfe43d6830bab9b401a0354e0ef7a916b3febe1
warum7 <warum7@warum7.com>
2023-02-28 18:25:06 +0100

    chore: add pinia

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,9 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     test: true,
-    ssr: false,
+    ssr: true,
     modules: [
         '@nuxtjs/tailwindcss',
-        'nuxt-vitest'
+        'nuxt-vitest',
+        '@pinia/nuxt'
     ],
 })
